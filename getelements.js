861d6b9eea2adc436623351a6659dc1abb082b9e
Steven VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-06-02 16:11:04 -0400

    solution

--- getelements.js
+++ getelements.js
@@ -3,12 +3,16 @@
 
 /* Get the element with an ID of 'first' */
 
+// const first = document.getElementById('first')
+// console.log(first)
 /* Now delete that first element (in the html file), and re-run your code. What changed? */
-
+const first = document.getElementById('first')
 /* Get the elements with a class of 'second' with getElementsByClassName: */
-
+const second = document.getElementsByClassName('second')
+console.log(second)
 /* Delete one of the elements with a class of 'second' and refresh the page. Do you get a similar result? */
  
-/* Get the span element using getElementsByTagname: */
-
+/* Get the span element using getElementsByTagName: */
+const spans = document.getElementsByTagName('span')
+console.log(span)
 /* Add a span anywhere, and re-run the last piece of code. What's different? */
--- queryselector.js
+++ queryselector.js
@@ -1,17 +1,24 @@
-
 // Get the element with an ID of 'first' using querySelector
-
+const first = document.querySelector('#first')
+// console.log(first)
 // Get the elements with a class of 'second' with querySelector
-
+const second = document.querySelector('.second')
+// console.log(second)
 // Try the above prompt with querySelectorAll. What's the difference between what these two methods return?
-
+const secondAll = document.querySelectorAll('.second')
+// console.log(secondAll)
 // Get the span element using querySelector
-
+const span = document.querySelector('span')
+// console.log(span)
 // Get multiple span elements using querySelectorAll
-
+const spanAll = document.querySelectorAll('span')
+// console.log(spanAll)
 // Select only "a" tags *directly inside* of a div (no grandchildren).
-
+const aTag = document.querySelector('div > a')
+// console.log(aTag)
 // Select all elements that contain a "data-target" attribute
-
+const dataTarget = document.querySelectorAll('[data-target]')
+// console.log(dataTarget)
 // Select all elements where the data-target attribute equals "#false"
-
+const falseDataTargets = document.querySelectorAll('[data-target="#false"]')
+// console.log(falseDataTargets)
