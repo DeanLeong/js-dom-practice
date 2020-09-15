d14246b289e46e15a22d106cc972a19ff50aa41f
Steven VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-09-15 13:17:06 -0600

    lab solution

--- queryselector.js
+++ queryselector.js
@@ -1,17 +1,27 @@
 
 // Get the element with an ID of 'first' using querySelector
-
+let first = document.querySelector('#first')
+// console.log(first)
 // Get the elements with a class of 'second' with querySelector
-
+// let secondClass = document.querySelector('.second')
+// console.log(secondClass)
 // Try the above prompt with querySelectorAll. What's the difference between what these two methods return?
-
+let secondClass = document.querySelectorAll('.second')
+// console.log(secondClass)
 // Get the span element using querySelector
-
+// let span = document.querySelector('span')
+// console.log(span)
 // Get multiple span elements using querySelectorAll
-
+// let span = document.querySelectorAll('span')
+// console.log(span)
 // Select only "a" tags *directly inside* of a div (no grandchildren).
-
+let aTag = document.querySelectorAll('div > a')
+// console.log("aTag",aTag)
+let aTags = document.querySelectorAll('div a')
+// console.log("aTags", aTags)
 // Select all elements that contain a "data-target" attribute
-
+let dataAtt = document.querySelectorAll('[data-target]')
+console.log(dataAtt)
 // Select all elements where the data-target attribute equals "#false"
-
+let falseDataAtt = document.querySelectorAll('[data-target="#false"]')
+console.log(falseDataAtt)
